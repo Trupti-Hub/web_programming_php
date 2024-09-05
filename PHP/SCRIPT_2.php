23690d6c53aac6fc8befb9d0976cefa42a635576
Trupti P <trupti.4267@gmail.com>
2024-09-05 21:15:09 +0530

    Add files via upload

---
+++ PHP/SCRIPT_2.php
@@ -0,0 +1,6 @@
+<?php
+$name = "TRUPTI";
+$age = 20;
+
+echo "My name is $name" . "<br>";
+echo "I am $age years old";
---
+++ PHP/SCRIPT_9.php
@@ -0,0 +1,5 @@
+<?php
+
+echo " Hello World" . "<br>";
+echo "Atmiya University" . "<br>";
+echo "MCA Student";
